e6467b5914ba7407228e5ba1cd1ecbc1ed2102d9
lm-afrin <titlee42@gmail.com>
2021-08-31 21:53:12 +0900

    settings for tailwind, router, axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import './styles/tailwind.css';
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+// axios.defaults.headers.common['Authorization'] = "Client-ID hBg14AILvY2jH_enddDGHpX6CTdf-u9MyxwHz8BlR5k"
+app.config.globalProperties.$axios = axios
+app.use(router)
+app.mount('#app')
