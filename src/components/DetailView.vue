895bc89286b044f893015df192e8057526a07a74
lm-afrin <titlee42@gmail.com>
2021-09-08 19:58:03 +0900

    added detail page

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -1,11 +1,10 @@
 <template>
-    <div>
+    <div v-if="obj">
         <h1>Details</h1>
-        <img :src="obj.urls['small']" >
+        <img :src="obj.urls.small">
         <p>{{obj.desc}}</p>
         <p>{{obj.likes}}</p>
         <p>{{obj.created_at}}</p>
-        {{obj.id}}
     </div>
 </template>
 
@@ -13,12 +12,33 @@
     export default {
         data() {
             return {
-                obj: {}
+                obj:null,
+                imgpath:null,
             }
         },
         props: ["item"],
+        computed: {
+            // imagePath: {
+            //     // return this.obj.urls['small']
+            //     get() {
+            //         return this.obj.urls['small']
+            //     },
+            //     set(value) {
+            //         this.obj.urls['small'] = value
+            //         return value
+            //     }
+            // }
+        },
         mounted() {
             this.obj = JSON.parse(this.$route.params.item)
+            
+            // if(this.obj){
+            //     let img=this.$refs.image;
+            //     img.src=this.obj.urls.small;
+            //     console.log(this.obj.urls,img)
+            // }
+            // console.log(this.obj.urls.small)
+            //this.imagePath = this.obj.urls.small
         }
     }
 </script>
