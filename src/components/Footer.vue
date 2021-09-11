ea295d9662106a6f563f0a65f07921bcca218762
lm-afrin <titlee42@gmail.com>
2021-09-11 14:49:01 +0900

    added footer to layout

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <footer class="bg-gray-200">
+            <p class="p-2 text-center text-xs">Copyright © Afrin (for-practise). All Rights Reserved.</p>
+        </footer>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <Navbar />
+        <Footer />
 
     </div>
 </template>
 
 <script>
     import Navbar from "../../components/Navbar.vue"
+    import Footer from "../../components/Footer.vue"
     export default {
         name: "Layout",
         components: {
             Navbar,
+            Footer,
         }
     }
 </script>
