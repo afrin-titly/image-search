b12d1f2ee7b1935fc8b037030f6dbe4b3d337e80
lm-afrin <titlee42@gmail.com>
2021-09-13 22:44:40 +0900

    separate search bar from home component

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,40 +1,6 @@
 <template>
   <div class="container mx-auto">
-    <div class="w-96 ml-96 py-4">
-      <div class="flex border-b py-2">
-        <input
-          class="
-            appearance-none
-            bg-transparent
-            border-none
-            w-full
-            text-gray-700
-            mr-3
-            py-1
-            px-2
-            leading-tight
-            focus:outline-none
-          "
-          type="text"
-          v-model="keyword"
-          @input="fetchAllImages(keyword)"
-        />
-        <button
-          class="
-            bg-blue-500
-            hover:bg-blue-700
-            text-white
-            font-bold
-            py-2
-            px-4
-            rounded
-          "
-          @click="fetchAllImages(keyword)"  
-        > 
-          Search
-        </button>
-      </div>
-    </div>
+    <SearchBar @keyword="fetchAllImages($event)" />
     <div class="grid grid-cols-3 gap-4">
       <ImageGrid :imageList="getAllImages[currentPage]" v-if="getAllImages" />
     </div>
@@ -58,12 +24,14 @@
 
 <script>
 // @ is an alias to /src
+import SearchBar from "../../components/SearchBar.vue"
 import ImageGrid from "../../components/ImageGrid.vue";
 import {mapActions, mapGetters} from "vuex"
 export default {
   name: "Home",
   components: {
     ImageGrid,
+    SearchBar,
   },
   data() {
     return {
